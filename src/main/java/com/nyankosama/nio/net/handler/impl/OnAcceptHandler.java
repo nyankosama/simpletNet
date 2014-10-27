7c1dc2ca79bc9dd526c824aff5e0bfaae65a9590
nyankosama <825138000@qq.com>
2014-10-27 15:32:56 +0800

    qps:1W2

--- src/main/java/com/nyankosama/nio/net/handler/impl/OnAcceptHandler.java
+++ src/main/java/com/nyankosama/nio/net/handler/impl/OnAcceptHandler.java
@@ -3,8 +3,6 @@ package com.nyankosama.nio.net.handler.impl;
 import com.nyankosama.nio.net.TcpConnection;
 import com.nyankosama.nio.net.callback.NetCallback;
 import com.nyankosama.nio.net.handler.SelectorHandler;
-import com.nyankosama.nio.net.utils.SynchronizedPrintln;
-import com.sun.org.apache.bcel.internal.generic.Select;
 
 import java.io.IOException;
 import java.nio.channels.SelectionKey;
@@ -36,20 +34,15 @@ public class OnAcceptHandler implements SelectorHandler{
 
     @Override
     public void process(SelectionKey key) throws IOException {
-//        SynchronizedPrintln.println("on accept");
         if (onMessageHandler == null) return;
         ServerSocketChannel channel = (ServerSocketChannel) key.channel();
         SocketChannel socketChannel = channel.accept();
-//        SynchronizedPrintln.println("channel == null: " + (socketChannel == null));
         socketChannel.configureBlocking(false);
-//        SynchronizedPrintln.println("set non-blocking end");
         socketChannel.register(selector, SelectionKey.OP_READ, onMessageHandler);
-//        SynchronizedPrintln.println("accept register end");
         if (onAcceptCallback != null) {
             TcpConnection tcpConnection = new TcpConnection(socketChannel, key);
             tcpConnection.reset(socketChannel, key);
             onAcceptCallback.onAccept(tcpConnection);
         }
-//        SynchronizedPrintln.println("on accept end");
     }
 }
--- src/main/java/com/nyankosama/nio/net/handler/impl/OnMessageHandler.java
+++ src/main/java/com/nyankosama/nio/net/handler/impl/OnMessageHandler.java
@@ -4,10 +4,8 @@ import com.nyankosama.nio.net.TcpBuffer;
 import com.nyankosama.nio.net.TcpConnection;
 import com.nyankosama.nio.net.callback.NetCallback;
 import com.nyankosama.nio.net.handler.SelectorHandler;
-import com.nyankosama.nio.net.utils.BindFunction;
 import com.nyankosama.nio.net.utils.ByteBufferThreadLocal;
 import com.nyankosama.nio.net.utils.NoCopyByteArrayOutputStream;
-import com.nyankosama.nio.net.utils.SynchronizedPrintln;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -15,8 +13,6 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by hlr@superid.cn on 2014/10/24.
@@ -31,11 +27,6 @@ public class OnMessageHandler implements SelectorHandler{
 
     private static final int WORK_QUEUE_CAPACITY = 100;
     private static final int WORK_THREAD_SIZE = Runtime.getRuntime().availableProcessors() + 1;
-
-    //for measurement
-    private AtomicInteger count = new AtomicInteger(0);
-    private long begin;
-    private long end;
 
     public OnMessageHandler() {
         this(null);
@@ -52,24 +43,16 @@ public class OnMessageHandler implements SelectorHandler{
 
     @Override
     public void process(SelectionKey key) {
-//        if (begin == 0) {
-//            begin = System.currentTimeMillis();
-//        }
         try {
             SocketChannel channel = (SocketChannel) key.channel();
             ByteBuffer buffer = ByteBufferThreadLocal.getInstance().get();
             NoCopyByteArrayOutputStream outputStream = new NoCopyByteArrayOutputStream(TcpBuffer.FIXED_BUFFER_SIZE);
-//            SynchronizedPrintln.println("read");
             int read = channel.read(buffer);
-//            SynchronizedPrintln.println("read down");
             if (read == -1) {
-                //handle close
-//                SynchronizedPrintln.println("on close");
                 channel.close();
                 key.cancel();
                 return;
             }
-//            SynchronizedPrintln.println("on message");
             do {
                 outputStream.write(buffer.array());
                 buffer.clear();
@@ -79,19 +62,8 @@ public class OnMessageHandler implements SelectorHandler{
                 TcpConnection tcpConnection = new TcpConnection(channel, key);
                 //NOTE TcpBuffer和ByteArrayOutputStream共享一个buf
                 TcpBuffer tcpBuffer = new TcpBuffer(outputStream.getBuf(), outputStream.size());
-//                callback.onMessage(tcpConnection, tcpBuffer);
-//                BindFunction messageFunc = BindFunction.bind(callback, "onMessage", tcpConnection, tcpBuffer);
-//                messageFunc.call();
                 workThreads[curWorkIndex = roundRobinIndex(curWorkIndex, WORK_THREAD_SIZE)]
                         .putWork(new InnerWork(tcpBuffer, tcpConnection, callback));
-//                SynchronizedPrintln.println("put work! threadIndex=" + curWorkIndex);
-//                count.incrementAndGet();
-//                if (count.get() % 10000 == 0) {
-//                    end = System.currentTimeMillis();
-//                    System.out.println("total handle time= " + (end - begin) + "ms");
-//                    long totalTime = workThreads[0].getTotalTimeNanos() / 1000000;
-//                    System.out.println("total wait time= " + totalTime + "ms");
-//                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -121,15 +93,12 @@ public class OnMessageHandler implements SelectorHandler{
 
     private static class InnerWorkThread extends Thread {
         private BlockingQueue<InnerWork> workQueue;
-        private long beforeTimeNanos;
-        private long totalTimeNanos;
 
         public InnerWorkThread() {
             this.workQueue = new ArrayBlockingQueue<>(WORK_QUEUE_CAPACITY);
         }
 
         public void putWork(InnerWork work) {
-            beforeTimeNanos = System.nanoTime();
             try {
                 workQueue.put(work);
             } catch (InterruptedException e) {
@@ -138,20 +107,12 @@ public class OnMessageHandler implements SelectorHandler{
             }
         }
 
-        public long getTotalTimeNanos() {
-            long result = totalTimeNanos;
-            totalTimeNanos = 0;
-            return  result;
-        }
-
         @Override
         public void run() {
             while (true){
                 try {
                     InnerWork work = workQueue.take();
-                    totalTimeNanos += System.nanoTime() - beforeTimeNanos;
                     work.call();
-//                    SynchronizedPrintln.println("work down thread=" + Thread.currentThread().getName());
                 } catch (InterruptedException e) {
                     //NOTE 忽略中断
                     e.printStackTrace();
