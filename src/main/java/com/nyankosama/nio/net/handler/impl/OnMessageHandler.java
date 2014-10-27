029135de43e9029b017fd79826c2c0c2e068a7b7
nyankosama <825138000@qq.com>
2014-10-27 15:16:29 +0800

    measure

--- src/main/java/com/nyankosama/nio/net/handler/impl/OnMessageHandler.java
+++ src/main/java/com/nyankosama/nio/net/handler/impl/OnMessageHandler.java
@@ -15,6 +15,8 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by hlr@superid.cn on 2014/10/24.
@@ -29,6 +31,11 @@ public class OnMessageHandler implements SelectorHandler{
 
     private static final int WORK_QUEUE_CAPACITY = 100;
     private static final int WORK_THREAD_SIZE = Runtime.getRuntime().availableProcessors() + 1;
+
+    //for measurement
+    private AtomicInteger count = new AtomicInteger(0);
+    private long begin;
+    private long end;
 
     public OnMessageHandler() {
         this(null);
@@ -45,6 +52,9 @@ public class OnMessageHandler implements SelectorHandler{
 
     @Override
     public void process(SelectionKey key) {
+        if (begin == 0) {
+            begin = System.currentTimeMillis();
+        }
         try {
             SocketChannel channel = (SocketChannel) key.channel();
             ByteBuffer buffer = ByteBufferThreadLocal.getInstance().get();
@@ -69,9 +79,18 @@ public class OnMessageHandler implements SelectorHandler{
                 TcpConnection tcpConnection = new TcpConnection(channel, key);
                 //NOTE TcpBuffer和ByteArrayOutputStream共享一个buf
                 TcpBuffer tcpBuffer = new TcpBuffer(outputStream.getBuf(), outputStream.size());
+//                callback.onMessage(tcpConnection, tcpBuffer);
                 BindFunction messageFunc = BindFunction.bind(callback, "onMessage", tcpConnection, tcpBuffer);
-                workThreads[curWorkIndex = roundRobinIndex(curWorkIndex, WORK_THREAD_SIZE)].putWork(messageFunc);
+                messageFunc.call();
+//                workThreads[curWorkIndex = roundRobinIndex(curWorkIndex, WORK_THREAD_SIZE)].putWork(messageFunc);
 //                SynchronizedPrintln.println("put work! threadIndex=" + curWorkIndex);
+                count.incrementAndGet();
+                if (count.get() % 10000 == 0) {
+                    end = System.currentTimeMillis();
+                    System.out.println("total handle time= " + (end - begin) + "ms");
+                    long totalTime = workThreads[0].getTotalTimeNanos() / 1000000;
+                    System.out.println("total wait time= " + totalTime + "ms");
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -85,12 +104,15 @@ public class OnMessageHandler implements SelectorHandler{
 
     private static class InnerWorkThread extends Thread {
         private BlockingQueue<BindFunction> workQueue;
+        private long beforeTimeNanos;
+        private long totalTimeNanos;
 
         public InnerWorkThread() {
             this.workQueue = new ArrayBlockingQueue<>(WORK_QUEUE_CAPACITY);
         }
 
         public void putWork(BindFunction function) {
+            beforeTimeNanos = System.nanoTime();
             try {
                 workQueue.put(function);
             } catch (InterruptedException e) {
@@ -99,11 +121,18 @@ public class OnMessageHandler implements SelectorHandler{
             }
         }
 
+        public long getTotalTimeNanos() {
+            long result = totalTimeNanos;
+            totalTimeNanos = 0;
+            return  result;
+        }
+
         @Override
         public void run() {
             while (true){
                 try {
                     BindFunction function = workQueue.take();
+                    totalTimeNanos += System.nanoTime() - beforeTimeNanos;
                     function.call();
 //                    SynchronizedPrintln.println("work down thread=" + Thread.currentThread().getName());
                 } catch (InterruptedException e) {
